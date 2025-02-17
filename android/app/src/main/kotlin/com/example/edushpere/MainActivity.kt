f303af495e13a0e98eade182f8dd63ac0a0def7a
ANJANA-V2003 <anjanavijayan832@gmail.com>
2025-02-17 13:01:21 +0530

    17-02-2025

---
+++ android/app/src/main/kotlin/com/example/edushpere/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.edushpere
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Teachers-module/Teacher_Login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TeacherLogin extends StatefulWidget {
+  const TeacherLogin({super.key});
+
+  @override
+  State<TeacherLogin> createState() => _TeacherLoginState();
+}
+
+class _TeacherLoginState extends State<TeacherLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}

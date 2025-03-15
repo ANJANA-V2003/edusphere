5b59f1465ed5e40b65d40c26585bfef46de6e507
ANJANA-V2003 <anjanavijayan832@gmail.com>
2025-03-15 20:46:41 +0530

    Teachers module

--- lib/Teachers-module/Teacher_home.dart
+++ lib/Teachers-module/Teacher_home.dart
@@ -58,9 +58,8 @@ class _Teacher_HomeState extends State<Teacher_Home> {
                                   SizedBox(
                                     width: 205.w,
                                   ),
-                                  GestureDetector(onTap: () {
-
-                                  },
+                                  GestureDetector(
+                                    onTap: () {},
                                     child: SvgPicture.asset(
                                         "assets/icons/lets-icons_bell-light.svg"),
                                   )
@@ -80,9 +79,232 @@ class _Teacher_HomeState extends State<Teacher_Home> {
                             ],
                           )
                         ],
-                      )
+                      ),
                     ],
                   ),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20.w, right: 20.w),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Search",
+                  hintStyle: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 15.sp,
+                      color: Colors.grey.shade600),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.r),
+                      borderSide: BorderSide(color: Colors.grey.shade600))),
+            ),
+          ),
+          SizedBox(
+            height: 15.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 150.h,
+                width: 390.w,
+                decoration: BoxDecoration(
+                    color: Color(0xff23ADB4),
+                    borderRadius: BorderRadius.circular(10.r)),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 20.w,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.h),
+                child: Text(
+                  "Explore Academics",
+                  style: GoogleFonts.poppins(
+                      fontSize: 20.sp, fontWeight: FontWeight.w600),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 175.h,
+                width: 175.w,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xff23ADB4)),
+                    borderRadius: BorderRadius.circular(10.r)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [SvgPicture.asset("assets/icons/quiz.svg")],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Create quiz",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.sp,
+                              color: Color(0xff23ADB4)),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "MCQ or written",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14.sp,
+                              color: Colors.grey.shade500),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 175.h,
+                width: 175.w,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xff23ADB4)),
+                    borderRadius: BorderRadius.circular(10.r)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [SvgPicture.asset("assets/icons/books.svg")],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "10 Total",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.sp,
+                              color: Color(0xff23ADB4)),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Homeworks pushed",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14.sp,
+                              color: Colors.grey.shade500),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 20.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 175.h,
+                width: 175.w,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xff23ADB4)),
+                    borderRadius: BorderRadius.circular(10.r)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [SvgPicture.asset("assets/icons/students.svg")],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "50 Total",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.sp,
+                              color: Color(0xff23ADB4)),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Students you have",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14.sp,
+                              color: Colors.grey.shade500),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 175.h,
+                width: 175.w,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Color(0xff23ADB4)),
+                    borderRadius: BorderRadius.circular(10.r)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [SvgPicture.asset("assets/icons/class.svg")],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "5 Total",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16.sp,
+                              color: Color(0xff23ADB4)),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Exams you took",
+                          style: GoogleFonts.poppins(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 14.sp,
+                              color: Colors.grey.shade500),
+                        )
+                      ],
+                    )
+                  ],
                 ),
               )
             ],

64502f7c3d9f091bc1df0c891cf609c5917e052b
pranave2003 <pranav2003a10@gmail.com>
2025-03-18 13:02:10 +0530

    firebase config successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
-import 'package:edushpere/Admin_module/admin_add_timetable.dart';
-import 'package:edushpere/Admin_module/admin_home.dart';
-import 'package:edushpere/Admin_module/admin_login.dart';
-import 'package:edushpere/Admin_module/admin_notice.dart';
-import 'package:edushpere/Admin_module/admin_teachers.dart';
 import 'package:edushpere/Teachers-module/Teacher_Login.dart';
-import 'package:edushpere/logo_splashscreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -20,30 +18,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
         designSize: Size(412, 892),
-    splitScreenMode: true,minTextAdapt: true,
-    builder: (context, child) =>MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: TeacherLogin(),
-    ));
+        splitScreenMode: true,
+        minTextAdapt: true,
+        builder: (context, child) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                // This is the theme of your application.
+                //
+                // TRY THIS: Try running your application with "flutter run". You'll see
+                // the application has a purple toolbar. Then, without quitting the app,
+                // try changing the seedColor in the colorScheme below to Colors.green
+                // and then invoke "hot reload" (save your changes or press the "hot
+                // reload" button in a Flutter-supported IDE, or press "r" if you used
+                // the command line to start the app).
+                //
+                // Notice that the counter didn't reset back to zero; the application
+                // state is not lost during the reload. To reset the state, use hot
+                // restart instead.
+                //
+                // This works for code too, not just values: Most code changes can be
+                // tested with just a hot reload.
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              home: TeacherLogin(),
+            ));
   }
 }

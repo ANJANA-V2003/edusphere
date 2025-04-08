3170501e60afb23010ed0f746540618b51c3dbc0
ANJANA-V2003 <anjanavijayan832@gmail.com>
2025-04-08 08:19:06 +0530

    parents navigationbar

--- lib/Parent_Students_module/parents_navigationbar.dart
+++ lib/Parent_Students_module/parents_navigationbar.dart
@@ -1,4 +1,7 @@
+import 'package:edushpere/Parent_Students_module/student_homepage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Parents_Navigationbar extends StatefulWidget {
   const Parents_Navigationbar({super.key});
@@ -8,8 +11,61 @@ class Parents_Navigationbar extends StatefulWidget {
 }
 
 class _Parents_NavigationbarState extends State<Parents_Navigationbar> {
+
+  int _Index = 0;
+  static const List<dynamic> _list = [
+    Student_Homepage(),
+    // Teacher_Calendar(),
+    // Teacher_Message(),
+    // Teacher_Profile()
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _Index = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold( body: _list.elementAt(_Index),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: "Home",
+              backgroundColor: Color(0xffD9D9D9)),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.date_range_outlined,
+
+              ),
+              label: "Quiz",
+              backgroundColor: Color(0xffD9D9D9)),
+          BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.chat_bubble_2_fill,
+              ),
+              label: "Message",
+              backgroundColor: Color(0xffD9D9D9)),
+          BottomNavigationBarItem(
+              icon: Icon(
+                CupertinoIcons.person,
+
+              ),
+              label: "Calendar",
+              backgroundColor: Color(0xffD9D9D9)),
+        ],
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _Index,
+        selectedItemColor:Color(0xff0B99A0),
+        selectedIconTheme: IconThemeData(color: Color(0xff0B99A0)),
+        selectedLabelStyle: GoogleFonts.poppins(
+          fontWeight: FontWeight.bold,),
+        iconSize: 30,
+        onTap: _onItemTapped,
+        elevation: 5,
+        backgroundColor: Colors.green[600],
+      ),);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:edushpere/Parent_Students_module/parents_navigationbar.dart';
 import 'package:edushpere/Parent_Students_module/splash_screen_parents.dart';
 import 'package:edushpere/Teachers-module/Teacher_Login.dart';
 import 'package:edushpere/Admin_module/logo_splashscreen_admin.dart';
@@ -46,7 +47,7 @@ class MyApp extends StatelessWidget {
                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
                 useMaterial3: true,
               ),
-              home: SplashScreen_Parents(),
+              home: Parents_Navigationbar(),
             ));
   }
 }
